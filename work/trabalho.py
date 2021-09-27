103fa63c94f767b9792df1b596cf8eb718e3972b
Celestino Kalueyo <chiponguekalueyo@yahoo.com>
2021-09-26 23:34:51 -0300

    Create trabalho.py

---
+++ work/trabalho.py
@@ -0,0 +1,33 @@
+
+print("ESCOLHA A OPÇÃO QUE ACHARES CORRETA")
+print("-="*25)
+print("""Opções
+[ 0 ] - Engenharia eletrica, engenharia Hidraulica
+[ 1 ] - Engenharia civil, engenharia Mecânica 
+[ 2 ] - Engenharia eletrica, Engenharia Eletrica
+[ 3 ] - Engenharia Biomedica, Engenharia Hidraulica
+[ 4 ] - Engenharia civil, Engenharia Hidraulica
+[ 5 ] - Engenharia Ambiental, Engenharia Hidraulica""")
+print("-="*25)
+curso = (int(input("Qual é o curso dos professores? ")))
+if curso == 0:
+    print("Opção incorreta, não estavas atento na primeira aula")
+    print("Pontuação: 0 ponto")
+elif curso == 1:
+    print("Opção incorreta, não estavas atento na primeira aula")
+    print("Pontuação: 0 ponto")
+elif curso == 2:
+    print("PARABENS, Você acertou")
+    print("Pontuação: 0.5 pontos")
+
+elif curso == 3:
+    print("Opção incorreta, não estavas atento na primeira aula")
+    print("Pontuação: 0 ponto")
+elif curso == 4:
+    print("Opção incorreta, não estavas atento na primeira aula")
+    print("Pontuação: 0 ponto")
+elif curso == 5:
+    print("Opção incorreta, não estavas atento na primeira aula")
+    print("Pontuação: 0 ponto")
+print("OBRIGADO PELA TENTAtIVA!")
+
